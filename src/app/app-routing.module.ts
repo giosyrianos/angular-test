a5d49d61917c1ab878bea645bb67e4a3b02feeb1
George Syrianos <gsyrianos@pavla.gr>
2021-01-15 13:48:09 +0200

    basic routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'property', loadChildren: () => import('./modules/property/property.module').then(m => m.PropertyModule) },
+
+  { path: '', component: LoginComponent},
+  {path: 'posts', component: PostListComponent},
+  { path: 'create', component: PostCreateComponent },
+  { path: 'edit/:postId', component: PostCreateComponent },
+  { path: 'sign-up', component: SignupComponent}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
