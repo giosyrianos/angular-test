bfcec05ef300b194f66b9296639a359674eb372d
George Syrianos <gsyrianos@pavla.gr>
2021-01-15 15:30:10 +0200

    feed server data to ui component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -9,11 +9,41 @@ import { Subscription } from 'rxjs';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
+  public posts: Post[] = [];
+  private postsSub: Subscription;
+  isLoading = false;
 
-  constructor() { }
+  totalPosts = 0;
+
+  currentPage = 1;
+
+  postsPerPage = 2;
+
+  pageSizeOptions = [1, 3, 4, 10];
+  constructor(
+    public postService: PostService
+  ) { }
 
   ngOnInit() {
+    this.isLoading = true;
+    this.postService.getPosts();
+    // this.postService.getPosts(this.postsPerPage, this.currentPage);
+    this.postsSub = this.postService.updatePostsListener()
+      .subscribe((postData: {posts: Post[]}) => {
+        this.isLoading = false;
+        this.posts = postData.posts;
+        // this will take posts count from back end in order to paginate data
+        this.totalPosts = this.posts.length;
+        console.log(this.posts);
+      });
+  }
+
+
+  ngOnDestroy()  {
+    // Called once, before the instance is destroyed.
+    // Add 'implements OnDestroy' to the class.
+    this.postsSub.unsubscribe();
   }
 
 }
